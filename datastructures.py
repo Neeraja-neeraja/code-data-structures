d893515a21c899c72e38b13fe3da19dc0d8924ec
Neeraja-neeraja <103265672+Neeraja-neeraja@users.noreply.github.com>
2022-04-18 12:57:28 +0530

    Update datastructures.py

--- datastructures.py
+++ datastructures.py
@@ -1,6 +1,6 @@
 #TASK  1
 p=3.14
-r=eval(input("Input the radius of the circle: "))
+r=int(input("Input the radius of the circle: "))
 area=p*r**2
 print("The area of the circle with radius",r, "is : ", area)
 
@@ -8,5 +8,5 @@ print("The area of the circle with radius",r, "is : ", area)
 #TASK 2
 filename=input("Input the filename: ")
 f_ext=filename.split(".")
-print("The extension of the file is " +repr(f_ext[-1]))
+print("The extension of the file is ",(f_ext[-1]))
 
